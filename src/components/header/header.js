712b919081e939dffe1a77cfddbb84ece83fbab9
Serik <serj_navii@mail.ru>
2021-06-05 06:03:20 +0600

    init: initial commit

--- src/components/header/header.js
+++
@@ -1,7 +0,0 @@
-import { BaseComponent } from '../base-component';
-
-export function Header(parentNode) {
-  const header = new BaseComponent(parentNode, 'header', { class: 'header' });
-  // eslint-disable-next-line no-new
-  new BaseComponent(header.element, 'h1', { class: 'header-title' }, 'Virtual Piano');
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { App } from './app';
-import './style.css';
-
-window.onload = () => {
-  App(document.body);
-};
